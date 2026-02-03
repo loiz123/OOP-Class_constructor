e6e8ac54583108323441b23dabf68ebfebabbd21
dinhquoc03032626 <dinhquoc03032626@gmail.com>
2026-02-03 12:22:05 +0700

    Add files via upload

---
+++ Sach.cs
@@ -0,0 +1,8 @@
+public class Sach
+{
+    public int IdSach { get; set; }
+    public string TenSach { get; set; }
+    public string TenTacGia { get; set; }
+    public DateTime NgayXuatBan { get; set; }
+    public string TinhTrang { get; set; }   // Ví dụ: "Con", "Het", "Hong"
+}
