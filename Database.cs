3468b58bf042398199ec5075aaf55b256b9d48fc
loiz123 <phatvv9@gmail.com>
2026-03-03 14:27:28 +0700

    Delete Database.cs

--- Database.cs
+++
@@ -1,6 +0,0 @@
-public static class Database
-{
-    public static List<Sach> DsSach = new();
-    public static List<TheLoai> DsTheLoai = new();
-    public static List<SachTheLoai> DsSachTheLoai = new();
-}
