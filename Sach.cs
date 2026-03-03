937e95609ca36d866617994fac03c1710bb9366d
loiz123 <phatvv9@gmail.com>
2026-03-03 14:28:14 +0700

    Delete Sach.cs

--- Sach.cs
+++
@@ -1,8 +0,0 @@
-public class Sach
-{
-    public int IdSach { get; set; }
-    public string TenSach { get; set; }
-    public string TenTacGia { get; set; }
-    public DateTime NgayXuatBan { get; set; }
-    public string TinhTrang { get; set; }   // Ví dụ: "Con", "Het", "Hong"
-}
